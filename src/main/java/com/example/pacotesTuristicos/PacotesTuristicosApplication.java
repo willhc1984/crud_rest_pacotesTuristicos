80f19b8031008936edc1063b26d23da8875c5f32
will henrique <willhc1984@gmail.com>
2022-07-04 14:11:44 -0300

    Inicio de projeto - IDE STS - configuração do projeto

---
+++ src/main/java/com/example/pacotesTuristicos/PacotesTuristicosApplication.java
@@ -0,0 +1,13 @@
+package com.example.pacotesTuristicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacotesTuristicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacotesTuristicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pacotesTuristicos/PacotesTuristicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pacotesTuristicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacotesTuristicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
