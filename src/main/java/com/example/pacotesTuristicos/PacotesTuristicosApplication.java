83279dfc55d354fa5df675ff9eb843a503431f45
unknown <willhc1984@gmail.com>
2022-07-06 13:55:47 -0300

    Populando tabela tb_restaurante - CommandLineRunner

--- src/main/java/com/example/pacotesTuristicos/PacotesTuristicosApplication.java
+++ src/main/java/com/example/pacotesTuristicos/PacotesTuristicosApplication.java
@@ -9,8 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.pacotesTuristicos.model.Cidade;
 import com.example.pacotesTuristicos.model.Hotel;
+import com.example.pacotesTuristicos.model.Restaurante;
 import com.example.pacotesTuristicos.repositories.CidadeRepository;
 import com.example.pacotesTuristicos.repositories.HotelRepository;
+import com.example.pacotesTuristicos.repositories.RestauranteRepository;
 
 @SpringBootApplication
 public class PacotesTuristicosApplication implements CommandLineRunner {
@@ -19,6 +21,8 @@ public class PacotesTuristicosApplication implements CommandLineRunner {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private HotelRepository hotelRepository;
+	@Autowired
+	private RestauranteRepository restauranteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PacotesTuristicosApplication.class, args);
@@ -40,8 +44,17 @@ public class PacotesTuristicosApplication implements CommandLineRunner {
 		Hotel hotel7 = new Hotel(null, "Ibis Hotel", 325.00, cidade3);
 		Hotel hotel8 = new Hotel(null, "All In Nacional", 325.00, cidade2);
 		
+		Restaurante r1 = new Restaurante(null, "Du chief", 564.00, cidade1);
+		Restaurante r2 = new Restaurante(null, "Du chief", 123.00, cidade2);
+		Restaurante r3 = new Restaurante(null, "Tempero Manero", 643.00, cidade3);
+		Restaurante r4 = new Restaurante(null, "Tempero Manero", 54.00, cidade4);
+		Restaurante r5 = new Restaurante(null, "Gaucho", 213.00, cidade4);
+		Restaurante r6 = new Restaurante(null, "Gaucho", 324.00, cidade2);
+		Restaurante r7 = new Restaurante(null, "Japa Grills", 234.00, cidade1);
+		
 		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3, cidade4));
 		hotelRepository.saveAll(Arrays.asList(hotel1, hotel2, hotel3, hotel4, hotel5, hotel6, hotel7, hotel8));
+		restauranteRepository.saveAll(Arrays.asList(r1, r2, r3, r4, r5, r6, r7));
 	}
 
 }
