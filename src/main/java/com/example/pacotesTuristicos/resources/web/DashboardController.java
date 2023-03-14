66ff8d7854329118d75128015d611ec50800e818
will henrique <willhc1984@gmail.com>
2023-03-14 14:36:47 -0300

    Ajustando links - thymeleaf fragments - area administrativa

---
+++ src/main/java/com/example/pacotesTuristicos/resources/web/DashboardController.java
@@ -0,0 +1,41 @@
+package com.example.pacotesTuristicos.resources.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/dashboard")
+public class DashboardController {
+	
+	@GetMapping
+	public String index() {
+		return "dashboard";
+	}
+	
+	@GetMapping(value = "/usuarios/cadastro")
+	public String cadusuarios() {
+		return "cad-usuarios";
+	}
+	
+	@GetMapping(value = "/pacotes/cadastro")
+	public String cadpacotes() {
+		return "cad-pacotes";
+	}
+	
+	@GetMapping(value = "/hoteis/cadastro")
+	public String cadhoteis() {
+		return "cad-hoteis";
+	}
+	
+	@GetMapping(value = "/cidades/cadastro")
+	public String cadcidades() {
+		return "cad-cidades";
+	}
+	
+	@GetMapping(value = "/restaurantes/cadastro")
+	public String cadrestaurantes() {
+		return "cad-restaurantes";
+	}
+
+}
