4462a0fa6c48fb5ddea1728bb33371198c2283db
will henrique <willhc1984@gmail.com>
2023-03-16 09:46:58 -0300

    Cidades - frontend/excluir

--- src/main/java/com/example/pacotesTuristicos/resources/web/CidadesController.java
+++ src/main/java/com/example/pacotesTuristicos/resources/web/CidadesController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -50,6 +52,19 @@ public class CidadesController {
 		cidadeService.salvar(cidade);
 		attr.addFlashAttribute("success", "Cidade cadastrada!");
 		return "redirect:/dashboard/cidades/cadastro";
+	}
+	
+	@GetMapping(value = "/excluir/{id}")
+	public String excluir(@PathVariable Integer id, RedirectAttributes attr) {
+		try {
+			cidadeService.apagar(id);
+			attr.addFlashAttribute("success", "Cidade excluida!");
+			return "redirect:/dashboard/cidades";	
+		} catch (Exception e) {
+			attr.addFlashAttribute("fail", "Erro de integridade de dados!");
+		}
+	
+		return "redirect:/dashboard/cidades";		
 	}
 
 }
